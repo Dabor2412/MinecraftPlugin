73883c70263f9bd99a5fafc17682f953e6ac260b
Dabor2412 <95739088+Dabor2412@users.noreply.github.com>
2023-02-17 18:01:53 +0100

    improved LoginEvent.java

--- src/main/java/me/dabor/dievincussy/eventhandlers/LoginEvent.java
+++ src/main/java/me/dabor/dievincussy/eventhandlers/LoginEvent.java
@@ -1,6 +1,11 @@
 package me.dabor.dievincussy.eventhandlers;
 
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.title.Title;
 import org.bukkit.ChatColor;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,6 +23,15 @@ public class LoginEvent implements Listener {
     public void onLogin(PlayerJoinEvent evt) throws SQLException, ClassNotFoundException {
         evt.getPlayer().sendMessage(ChatColor.AQUA + "Hallo " + evt.getPlayer().getName());
         executeSQL(evt.getPlayer());
+
+        Component mainTitle = Component.text("Wilkommen zu ", TextColor.color(0x43A5AE)).append(Component.text("Wildlands", TextColor.color(0xFFFD55)));
+        Component subTitle = Component.text("von Noel, Noah und Vincent", TextColor.color(0xB381));
+
+        Title title = Title.title(mainTitle, subTitle);
+        Audience audience = Audience.audience(evt.getPlayer());             //so viel verschwendete Zeit
+        audience.showTitle(title);
+
+        evt.getPlayer().playSound(evt.getPlayer().getLocation(), Sound.ENTITY_ENDER_DRAGON_GROWL, 1f, 1f);
     }
 
     private void executeSQL(Player player) throws ClassNotFoundException, SQLException {
